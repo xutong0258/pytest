678e57f325457d7f1ed738277d3f0cadb57482a3
xutong0258 <123801613+xutong0258@users.noreply.github.com>
2024-06-12 19:15:19 +0800

    Add files via upload

---
+++ test_class.py
@@ -0,0 +1,8 @@
+import pytest
+
+class TestClass:
+    def test_method1(self):
+        assert 1 + 1 == 2
+
+    def test_method2(self):
+        assert 2 * 3 == 6
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+def test_case1():
+    assert 1==2
+
+def test_case2():
+    assert 1==2
